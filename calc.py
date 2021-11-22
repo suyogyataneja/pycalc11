f6966a8d9e4ee8fd301127a088e4c7ef467c28ff
Suyogya <suyogyataneja@gmail.com>
2021-11-23 10:36:21 +1100

    Initial work on a Python string calculator

---
+++ calc.py
@@ -0,0 +1,10 @@
+def compute(exp):
+	values = exp.split(' ')
+	num0= int (values[0])
+	operator = values[1]
+	num1 = int(values)
+	if operator == '+':
+		return num0 + num1
+	else:
+		print('unkown operator!')
+		return 0 
