92f293db83211fe028bda8c61e3ade3f535ac90e
Suyogya <suyogyataneja@gmail.com>
2021-11-23 11:47:23 +1100

    changes done

--- calc.py
+++ calc.py
@@ -1,10 +1,12 @@
-def compute(exp):
-	values = exp.split(' ')
-	num0= int (values[0])
-	operator = values[1]
-	num1 = int(values)
-	if operator == '+':
-		return num0 + num1
-	else:
-		print('unkown operator!')
-		return 0 
+def compute(expression):
+    values = expression.split(' ')
+    num0 = int(values[0])
+    operator = values[1]
+    num1 = int(values[2])
+    if operator == '+':
+        return num0 + num1
+    elif operator == '-':
+        return num0 - num1
+    else:
+        print('unknown operator!')
+        return None
